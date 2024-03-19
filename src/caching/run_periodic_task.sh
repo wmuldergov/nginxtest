6bebf1a1cb7edf525d72ea0796ca131585781777
Mulder <Willem.Mulder@gov.bc.ca>
2024-03-19 14:56:18 -0700

    test

---
+++ src/caching/run_periodic_task.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Function to run the command
+run_periodic_task() {
+    run-parts /etc/periodic/15min
+}
+
+# Infinite loop to run the command every minute
+while true; do
+    run_periodic_task
+    sleep 60
+done
